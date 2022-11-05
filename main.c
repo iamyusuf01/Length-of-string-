abc6bbe29fc76767f599c85c5f49457337790fa6
Md Yusuf Ansari <100552609+iamyusuf01@users.noreply.github.com>
2022-11-05 09:48:54 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+  char str[20];
+  int i;
+  printf("enter your name ");
+    gets(str);
+  i=strlen(str);
+  printf("length is ,%d",i);
+ 
+  
+}
